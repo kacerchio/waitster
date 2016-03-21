0f3be217a1db49bcb058dcb085afc7543e667fa4
Kristel Tan <ktan@bu.edu>
2016-03-21 15:45:10 -0400

    Index.js Update

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         var address = $("#search-input").val();
         $.getJSON(deliveryAPI + " &address=" + address, function(json) {
             alert("Got JSON Data");
-            //console.log(json[Object.keys(json[])]);
+            console.log(json[Object.keys(json)[4]]);
             console.log(json);
         });
     });
