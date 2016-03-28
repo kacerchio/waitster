34a92b226e0c3377ee9430d1b683106a37971fbf
Kristel Tan <ktan@bu.edu>
2016-03-27 20:53:42 -0400

    Fixed Geolocation Function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,55 +1,52 @@
-$(document).ready(function(){
+var deliveryAPI = "https://api.delivery.com/merchant/search/delivery?client_id=YTExMWZlN2E1MTE4YjI1MGM4MzFlYzkzMTM5YzBkN2Uy";
+var googleAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
+var googleKey = "&key=AIzaSyCnbPRMgv_MDYaPqiq2mVYIpWUy-m_k3Jc";
 
-    var deliveryAPI = "https://api.delivery.com/merchant/search/delivery?client_id=YTExMWZlN2E1MTE4YjI1MGM4MzFlYzkzMTM5YzBkN2Uy";
-    var googleAPI = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBdBMLBqrTp0Yp2zH3z5HhZATlBKTAuejM&callback=initMap"
+function search() {
 
-    $("#search-btn").click(function(){
-        var address = $("#search-input").val();
-        var queryURL = deliveryAPI + " &address=" + address;
-        $.getJSON(queryURL, function(json) {
+    var query = $("#search-input").val();
+
+    if (validateAddress(query) == true) {
+        var addressQuery = deliveryAPI + " &address=" + query;
+        $.getJSON(addressQuery, function (json) {
             alert("Got JSON Data");
             var i;
-            for (i=0; i < json.merchants.length; i++) {
+            for (i = 0; i < json.merchants.length; i++) {
                 if (json.merchants[i]["summary"]["type_label"].localeCompare("Restaurant") == 0) {
                     console.log(json.merchants[i]["summary"]);
+                    console.log("Last Delivery Time: " + json.merchants[i]["ordering"]["availability"]["last_delivery_time"]);
                 }
-
-                console.log("Last Delivery Time: " + json.merchants[i]["ordering"]["availability"]["last_delivery_time"]);
             }
         });
-    });
+    }
+}
 
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 6
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});
+function validateRestName(name) {
+    var regexp = /^[a-z\d\-_\s]+$/i;
+    return regexp.test(name);
+}
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
+function validateAddress(address) {
+    var regexp = /^[a-zA-Z0-9\s,'-]*$/;
+    return regexp.test(address);
+}
 
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('Location found.');
-                map.setCenter(pos);
-            }, function() {
-                handleLocationError(true, infoWindow, map.getCenter());
+var lat, lng, address;
+
+function loadLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
+            var latlngQuery = googleAPI + lat + "," + lng + googleKey;
+            $.getJSON(latlngQuery, function(json) {
+                address = json.results[0]["formatted_address"];
+                $("#search-input").val(address);
             });
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
     }
+}
 
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
-    }
-});
+window.onload = loadLocation();
